e9ce258a099045530926b123531922d6e631e496
kim changduk <kimchangduk92@gmail.com>
2018-06-01 16:09:14 +0900

    학습데이터 json으로 저장

--- src/YoutubeSubtitleSplitter/Program.cs
+++ src/YoutubeSubtitleSplitter/Program.cs
@@ -6,16 +6,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using NAudio.Wave;
+using Newtonsoft.Json;
 
 namespace YoutubeSubtitleSplitter
 {
     class Program
     {
+        private static List<SplitInformation> splitInformations = new List<SplitInformation>();
+        private static readonly string resultJsonFileName = "result.json";
+
         static void Main(string[] args)
         {
+            LoadResultJson();
             Console.WriteLine("Type *.wav path.");
-            // string directory = Console.ReadLine();
-            string directory = @"G:\workspace\github\youtube-downloader\src\YouTubeDownloader\bin\Debug\test";
+            string directory = Console.ReadLine();
             var files = Directory.GetFiles(directory);
             foreach (var wavName in files.Where(f => f.EndsWith(".wav")))
             {
@@ -24,6 +28,24 @@ namespace YoutubeSubtitleSplitter
                 if (File.Exists(xmlName))
                 {
                     Spit(itemName, wavName, xmlName);
+                }
+            }
+        }
+
+        private static void LoadResultJson()
+        {
+            if (File.Exists(resultJsonFileName))
+            {
+                try
+                {
+                    var json = File.ReadAllText(resultJsonFileName);
+                    splitInformations = JsonConvert.DeserializeObject<List<SplitInformation>>(json);
+                    Console.WriteLine($"{resultJsonFileName} Loaded!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{resultJsonFileName} Load Failed!");
+                    throw;
                 }
             }
         }
@@ -42,6 +64,7 @@ namespace YoutubeSubtitleSplitter
                 long prevEndTime = 0;
                 foreach (XmlNode node in nodes)
                 {
+                    string sentense = node.InnerText;
                     var startTime = Int64.Parse(node.Attributes["t"].Value);
                     var endTime = startTime + Int64.Parse(node.Attributes["d"].Value);
                     if (startTime < prevEndTime + 300)
@@ -71,7 +94,28 @@ namespace YoutubeSubtitleSplitter
                         }
                     }
                     prevEndTime = endTime;
+                    splitInformations.Add(new SplitInformation(saveFileName,sentense));
                 }
+            }
+            SaveSplitInformation();
+        }
+
+        private static void SaveSplitInformation()
+        {
+            File.WriteAllText(resultJsonFileName, Newtonsoft.Json.JsonConvert.SerializeObject(splitInformations));
+        }
+
+        private class SplitInformation
+        {
+            [JsonProperty("text")]
+            public string Text { get; set; }
+            [JsonProperty("file")]
+            public string File { get; set; }
+
+            public SplitInformation(string fileName,string text)
+            {
+                this.File = fileName;
+                this.Text = text;
             }
         }
     }
